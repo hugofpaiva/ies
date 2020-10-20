b9233ec63279bf309e950e38e1aeb9b7d298341e
hugofpaiva <hugofpaiva@ua.pt>
2020-10-20 22:50:03 +0100

    Último exercício e ver id da cidade através do nome

--- Prática/Prática 1/1.3/postgreSQL/run.sh
+++ Prática/Prática 1/1.3/postgreSQL/run.sh
@@ -3,11 +3,15 @@ docker build -t eg_postgresql .
 
 docker rm -f pg_test
 
-docker volume create postgres_data
+docker volume create postgres_data-postgresql
+docker volume create postgres_data-log
+docker volume create postgres_data-lib
 
 docker run -d \
   --restart unless-stopped \
   --name pg_test \
-  -v postgres_data:/etc/postgresql/9.3/main/ \
+  -v postgres_data-postgresql:/etc/postgresql \
+  -v postgres_data-log:/var/log/postgresql \
+  -v postgres_data-lib:/var/lib/postgresql \
   -p 5432:5432 \
   eg_postgresql
