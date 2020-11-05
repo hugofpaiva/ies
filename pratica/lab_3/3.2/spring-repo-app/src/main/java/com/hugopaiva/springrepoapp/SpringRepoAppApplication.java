3bac738ea174b958637fed515116d52a0858fad4
hugofpaiva <hugofpaiva@ua.pt>
2020-11-05 10:52:06 +0000

    Começo Lab3

---
+++ pratica/lab_3/3.2/spring-repo-app/src/main/java/com/hugopaiva/springrepoapp/SpringRepoAppApplication.java
@@ -0,0 +1,13 @@
+package com.hugopaiva.springrepoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRepoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRepoAppApplication.class, args);
+	}
+
+}
---
+++ pratica/lab_3/3.2/spring-repo-app/src/test/java/com/hugopaiva/springrepoapp/SpringRepoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugopaiva.springrepoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRepoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
